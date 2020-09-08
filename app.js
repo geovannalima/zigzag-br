33e48eb605938ec4d9fb36a8ea29fa8dbea7df7c
Geovanna <geovannalima1562@gmail.com>
2020-09-08 14:13:43 -0300

    Importação

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const HomeRoute = require("./app/routes/home.js");
 var app = require("./config/server");
+
 app.set("view engine", "ejs");
+
 app.set("views", "./app/views");
+
 HomeRoute(app);
 app.listen(5000, function () {
-  console.log("Servidor on");
+	console.log("Servidor on");
 });
-<<<<<<< HEAD
 var app = require('./config/server');
 
 app.listen(5000, function(){
 	console.log("Servidor on");
 });
-=======
->>>>>>> a956aa1... Importação
--- app/routes/pesquisa_giria.js
+++ app/routes/pesquisa_giria.js
@@ -1,10 +1,7 @@
 module.exports = function (app) {
-<<<<<<< HEAD
   app.get('/buscar', function (req, res) {
     res.render('girias/pesquisa');
-=======
   app.get("/buscar", function (req, res) {
     res.render("girias/pesquisa");
->>>>>>> a956aa1... Importação
   });
 };
