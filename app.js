d42787e99aed8d4884196fe78861b22319fb4e8a
Geovanna <geovannalima1562@gmail.com>
2020-10-18 20:38:49 -0300

    Segundo commit: syle.css, giria.ejs e select

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ app.set("view engine", "ejs");
 app.set("views", "./app/views");
 
 HomeRoute(app);
+<<<<<<< HEAD
 app.listen(5000, function () {
 	console.log("Servidor on");
+=======
+app.listen(5000, function (){
+  console.log("Servidor on");
+>>>>>>> cfdcb54... Segundo commit: syle.css, giria.ejs e select
 });
 var app = require('./config/server');
 
